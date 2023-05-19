92f33bd5b3bb3fb93a25a571d512696039264a96
Monarch <monarchofaether@gmail.com>
2023-05-19 22:18:12 +0100

    printf string constant edited again

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,8 +19,8 @@ int main(void)
 	if (m > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, m);
 	else if (m == 0)
-		printf("The last digit of %d is %d and is 0\n", n, m);
+		printf("Last digit of %d is %d and is 0\n", n, m);
 	else if (m < 6 && m != 0)
-		printf("The last digit of %d is %d and is less than 5\n", n, m);
+		printf("Last digit of %d is %d and is less than 5\n", n, m);
 	return (0);
 }
