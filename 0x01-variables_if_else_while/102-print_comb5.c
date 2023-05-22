261c115e3ace3c1c25273a92085c80227a259f6c
Monarch <monarchofaether@gmail.com>
2023-05-22 15:17:43 +0100

    not correct output but testing for file 102-print_comb.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,39 +8,34 @@
 
 int main(void)
 {
-	int c, j, k;
+	int c, j, k, l;
 
 	for (c = 48; c <= 57; c++)
 	{
 		for (j = 48; j <= 57; j++)
 		{
-			putchar(' ');
 			for (k = 48; k <= 57; k++)
 			{
 				for (l = 48; l <= 57; l++)
 				{
-					if ((c == j && j == k) ||
-(c > j && j > k) ||(c > j && j < k) || (c > j && j == k) || (c == j && j > k) ||
-(c == j && j < k) || (c == k && j < k) || (c == k && j > k) || (c < j && j == k)
-||(c < k && k == j) || (c < j && j > k))
-						;
-					else
+					if ((j < l) && j != l)
 					{
 						putchar(c);
 						putchar(j);
 						putchar(k);
-
-						if (!(c == 55 && j == 56 && k == 57))
-						{
-							putchar(',');
-							putchar(' ');
+						putchar(l);
+						if (!(c == 54 && j == 55 &&
+						      k == 56 && l == 57))
+                                                {
+                                                        putchar(',');
+                                                        putchar(' ');
+                                                }
 					}
 				}
 			}
 		}
 	}
 
-   }
 	putchar('\n');
 	return (0);
 }
