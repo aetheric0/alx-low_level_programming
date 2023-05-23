524184df0fe8f4d29288c446abbeaaf330b2b9d7
Monarch <monarchofaether@gmail.com>
2023-05-23 23:23:49 +0100

    spaces between corrected and trailing space and comma removed

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,14 +21,21 @@ void times_table(void)
                         {
                                 _putchar(k / 10 + '0');
                                 _putchar(k % 10 + '0');
+				if (j != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
                         }
                         else
-                        _putchar(k + '0');
-                        if (!(j == 9))
-			{
-                        _putchar(',');
-                        _putchar(' ');
-                        _putchar(' ');
+                        {
+				_putchar(k + '0');
+				if (j != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
 			}
                 }
         }
