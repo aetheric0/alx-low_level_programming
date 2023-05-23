8b7ad0373a235513d61d4e4b30acfd14141755ee
Monarch <monarchofaether@gmail.com>
2023-05-23 22:01:10 +0100

    betty enforced

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * _isalpha - writes the the string _putchar to stdout
  *
  * @c: receives an int converted to char
- "
+ *
  * Return: On success 0.
  * On error, -1 is returned, and errno is set appropriately.
  */
@@ -16,9 +16,9 @@ int _isalpha(int c)
 
 	for (i = 65; i <= 122; i++)
 	{
-		if ( i >= 91 && i <= 96)
+		if (i >= 91 && i <= 96)
 			a = 0;
-		else if( c == i)
+		else if (c == i)
 		{
 			a = 1;
 			break;
