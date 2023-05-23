b107d5976f5816bfbeb5c00989444a69a5037345
Monarch <monarchofaether@gmail.com>
2023-05-23 21:00:35 +0100

    function definitions removed from main.h

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,9 +1,28 @@
 #include "main.h"
 
-void times_table(void);
-
-int main(void)
+void times_table(void)
 {
-	times_table();
-	return (0);
+        int i, j, k;
+
+        for (i = 0; i <= 9; i++)
+        {
+                _putchar('\n');
+                for (j = 0; j <= 9; j++)
+                {
+                        k = i * j;
+                        if (k > 9)
+                        {
+                                _putchar(k / 10 + '0');
+                                _putchar(k % 10 + '0');
+                        }
+                        else
+                        _putchar(k + '0');
+                        if (!(j == 9))
+                        _putchar(',');
+                        _putchar(' ');
+                        _putchar(' ');
+                }
+        }
+        _putchar('\n');
+        return;
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,6 @@
 #ifndef MY_HEADER_FILE_H
 #define MY_HEADER_FILE_H
 
-#include <unistd.h>
-
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -13,32 +11,5 @@
 int _putchar(char c);
 void print_alphabet(void);
 void times_table(void);
-
-void times_table(void)
-{
-	int i, j, k;
-
-	for (i = 0; i <= 9; i++)
-	{
-		_putchar('\n');
-		for (j = 0; j <= 9; j++)
-		{
-			k = i * j;
-			if (k > 9)
-			{
-				_putchar(k / 10 + '0');
-				_putchar(k % 10 + '0');
-			}
-			else
-			_putchar(k + '0');
-			if (!(j == 9))
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-		}
-	}
-	_putchar('\n');
-	return;
-}
 
 #endif
