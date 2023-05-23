0d3b18e2b3c4c7db73040b73f024adc149cbb742
Monarch <monarchofaether@gmail.com>
2023-05-23 20:41:38 +0100

    comment modified to betty style

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - writes the the string _putchar to stdout
  *
  * Return: On success 0.
  * On error, -1 is returned, and errno is set appropriately.
