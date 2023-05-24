72d92ef6b94b414c7b159f33e61569638889f160
Monarch <monarchofaether@gmail.com>
2023-05-24 09:24:07 +0100

    print every second, minute and hour of every day in 00:00 format

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,3 +6,41 @@
  * Return: On success 0.
  * On error, -1 is returned, and errno is set appropriately.
  */
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 48; i <= 50; i++)
+	{
+		for (j = 48; j <= 59; j++)
+		{
+			for (k = 48; k <= 53; k++)
+			{
+				for(l = 48; l <= 57; l++)
+				{
+					if (i == 50 && j > 51)
+						;
+					else if (i < 50 &&j <= 57)
+					{
+					_putchar(i);
+					_putchar(j);
+					_putchar(':');
+					_putchar(k);
+					_putchar(l);
+					_putchar('\n');
+					}
+					else if (i == 50 && j <= 51)
+					{
+					_putchar(i);
+					_putchar(j);
+					_putchar(':');
+					_putchar(k);
+					_putchar(l);
+					_putchar('\n');
+
+					}
+				}
+			}
+		}
+	}
+}
