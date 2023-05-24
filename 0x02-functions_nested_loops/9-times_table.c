bcb1a16270a6b788d97af489d2218099b2fb8760
Monarch <monarchofaether@gmail.com>
2023-05-24 03:37:59 +0100

    logic reconfigured

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,44 +1,30 @@
 #include "main.h"
 
 /**
- * times_table - writes the the string _putchar to stdout
- *
- * Return: On success 0.
- * On error, -1 is returned, and errno is set appropriately.
+ *times_table - Prints the 9 times table
  */
-
 void times_table(void)
 {
-        int i, j, k;
+	int i, j, k;
 
-        for (i = 0; i <= 9; i++)
-        {
-                _putchar('\n');
-                for (j = 0; j <= 9; j++)
-                {
-                        k = i * j;
-                        if (k > 9)
-                        {
-                                _putchar(k / 10 + '0');
-                                _putchar(k % 10 + '0');
-				if (j != 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-                        }
-                        else
-                        {
-				_putchar(k + '0');
-				if (j != 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-				}
-			}
-                }
-        }
-        _putchar('\n');
-        return;
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0');
+
+		for (j = 1; j <= 9; j++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			k = i * j;
+
+			if (k <= 9)
+				_putchar(' ');
+			else
+				_putchar((k / 10) + '0');
+
+			_putchar((k % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
