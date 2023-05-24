833fe7d683c7e654b3515b25ed7dd9d37402bf3a
Monarch <monarchofaether@gmail.com>
2023-05-24 19:56:38 +0100

    natural numbers 101-natural.c file added

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * main - prints the added numbers
+ * Return: value 0 is successful
+ * print_nat: function that adds and prints the number
+*/
+
+int main(void)
+{
+	print_nat();
+	return (0);
+}
+
+void print_nat(void)
+{
+	int i,j,c,k;
+
+	i = 0;
+	j = 0;
+	while (i < 1024)
+	{
+		if (((i % 3) == 0) || ((i % 5) == 0))
+		{
+			j = j + i;
+		}
+		i++;
+	}
+	for(c = 100000; c >= 1; c = c / 10)
+	{
+		k = j / c;
+		if (k < 9)
+			_putchar(k + '0');
+		if (k > 9)
+		{
+			k = k % 10;
+			_putchar(k + '0');
+		}
+	}
+	_putchar('\n');
+}
