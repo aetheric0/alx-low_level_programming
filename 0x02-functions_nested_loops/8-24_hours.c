b4f36e8c01cce3363677b4d96938d2607627b676
Monarch <monarchofaether@gmail.com>
2023-05-24 09:26:24 +0100

    betty coding style enforced

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,11 +16,11 @@ void jack_bauer(void)
 		{
 			for (k = 48; k <= 53; k++)
 			{
-				for(l = 48; l <= 57; l++)
+				for (l = 48; l <= 57; l++)
 				{
 					if (i == 50 && j > 51)
 						;
-					else if (i < 50 &&j <= 57)
+					else if (i < 50 && j <= 57)
 					{
 					_putchar(i);
 					_putchar(j);
