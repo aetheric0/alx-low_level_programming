9ec9b66d916de4a59f93b51ffe53a19446ec9cbf
Monarch <monarchofaether@gmail.com>
2023-05-24 20:58:50 +0100

    fibonacci file written

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,12 +2,22 @@
 
 int main(void)
 {
-}
+	int i, j;
 
-void Fib(void)
-{
-	int i;
-	
-	for (i = 0; i <= 50; i++)
-		
+	j = 0;
+	for (i = 1; i <= 50; i++)
+	{
+		j = j + i;
+		if (j < 9)
+			_putchar(j + '0');
+		else if (j > 9)
+		{
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+		}
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar('\n');
+	return (0);
 }
