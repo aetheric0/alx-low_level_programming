63d78b6e0625a29687531a6f9d97608e0b065f13
Monarch <monarchofaether@gmail.com>
2023-05-25 20:52:45 +0100

    logic of program changed

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,14 +8,11 @@
 
 int _isupper(int c)
 {
-	int i;
-
-	for (i = 65; i <= 90; i++)
+	int _isupper(int c)
 	{
-		if (c == i)
-		{
+		if (c >= 'A' && c <= 'Z')
 			return (1);
-		}
 		else
 			return (0);
+	}
 }
