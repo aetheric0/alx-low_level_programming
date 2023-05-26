de425f6563dbe6bf58b7ec3285b52519097607dc
Monarch <monarchofaether@gmail.com>
2023-05-26 02:03:12 +0100

    FizzBuzz program made

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - FizzBuzz program
+ *
+ * Return:  0 is (success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 15) == 0)
+			printf("FizzBuzz ");
+		else if ((i % 3) == 0)
+			printf("Fizz ");
+		else if ((i % 5) == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	putchar('\n');
+
+	return (0);
+}
