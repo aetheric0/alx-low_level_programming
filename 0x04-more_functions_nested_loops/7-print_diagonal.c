9c2674187ec7fcd15c75b2b1abd367e9bb7acd86
Monarch <monarchofaether@gmail.com>
2023-05-27 17:06:00 +0100

    else statement added

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,14 +14,16 @@ void print_diagonal(int n)
 	{
 		;
 	}
-	
-	for (i = 0; i < n; i++)
+	else
 	{
-		for (j = 0; j < i; j++)
-		      {
-			      _putchar(' ');
-		      }
-		_putchar('\\');
-		_putchar('\n');
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
 	}
 }
