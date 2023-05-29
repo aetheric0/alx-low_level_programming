34ccd1285df4247c74c9f8c68d1ad1a90a36f105
Monarch <monarchofaether@gmail.com>
2023-05-29 21:04:43 +0100

    function to update value of pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * void reset_to_98(int *n) - updates value of pointer
+ * @n: integer pointer value passed to function
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
