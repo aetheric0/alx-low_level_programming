23083516a948fb76e6c6738d9ebc05abe842dc82
Monarch <monarchofaether@gmail.com>
2023-05-29 23:18:28 +0100

    length of string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,20 @@
 #include "main.h"
 
 /**
- * _strlen - updates value of pointer
- * @a: first pointer param
- * @b: second pointer param
- * Return: Nothing
+ * _strlen - gets the length lf the string
+ * @s: parameter is the variable that hold the string
+ * Return: length of string
  */
 
 int _strlen(char *s)
 {
 	int c = 0;
 
-	c = sizeof(s);
+	while (*s != '\0')
+	{
+		c++;
+		s++;
+	}
 
 	return (c);
 }
