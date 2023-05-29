78d31e484229a56282423dbc34b6ba6560087ab0
Monarch <monarchofaether@gmail.com>
2023-05-29 21:10:53 +0100

    parameter in description removed

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98(int *n) - updates value of pointer
+ * reset_to_98 - updates value of pointer
  * @n: integer pointer value passed to function
  * Return: Nothing
  */
