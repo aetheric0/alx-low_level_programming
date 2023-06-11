a7c3155b8f28c05ba038ce065462c47923c51bad
Monarch <monarchofaether@gmail.com>
2023-06-11 18:11:55 +0200

    0-strcat.c - function that concatenates two string, 0-main.c - test file, 0-strcat - compiled program

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,29 @@
-#include <stdio.h>
-
+#include "main.h"
+/**
+ * _strcat -  Function to concatenate two strings
+ * @dest: first pointer argument
+ * @src: second pointer argument
+ * Return -  Type Char
+ */
 char *_strcat(char *dest, char *src)
 {
+	int i, j, k, len;
+
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
+	src = &dest[i+1];
+
+	j = 0;
+	while (src[j] != '\0')
+		j++;
+	j++;
+
+	len = i + j;
+
+	k = 0;
+	for (k = 0; k < len; k++)
+		dest = &dest[0 + k];
+
+	return (dest);
 }
