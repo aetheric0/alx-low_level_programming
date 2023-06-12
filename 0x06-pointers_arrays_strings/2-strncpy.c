ccb916a900a4067cfa07919b216e4562b68bf90a
Monarch <monarchofaether@gmail.com>
2023-06-12 05:56:12 +0200

    fills in the remaining characters of the first argument after the second argument is copied

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,9 +4,12 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; src[i] != '\0' && i < n; i++)
+	for (i = 0; src[i] != '\0'; i++)
 	{
-		dest[i] = src[i];
+		if (i < n)
+			dest[i] = src[i];
+		else
+			dest[i] = dest[i];
 	}
 	dest[i] = '\0';
 
