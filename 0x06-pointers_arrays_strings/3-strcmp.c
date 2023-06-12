cc5ff6bbf62ea12c3560072e4f187a111544d2ef
Monarch <monarchofaether@gmail.com>
2023-06-12 14:20:22 +0200

    description comments added

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * _strcmp = program to compare two strings
+ * @s1: first argument - takes string to be compared
+ * @s2: second argument - takes string to be compared with
+ * Return: Returns the value of the compared string in decimal
+ */
+
 int _strcmp(char *s1, char *s2)
 {
 	int i, j, res;
