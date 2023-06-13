4b5b8e9222899e272c6b88cff86ac6db344f602c
Monarch <monarchofaether@gmail.com>
2023-06-13 19:39:20 +0200

    changes made to suit betty styling

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,7 +2,7 @@
 
 /**
  * *string_toupper - function that changes lower case characters to uppercase
- * @*: argument containing the character to change
+ * @*s: argument containing the character to change
  * Return: returns converted character
  */
 
@@ -11,9 +11,9 @@ char *string_toupper(char *s)
 	int i;
 	char a;
 
-	for(i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for(a = 97; a <= 122; a++)
+		for (a = 97; a <= 122; a++)
 		{
 			if (s[i] == a)
 			{
