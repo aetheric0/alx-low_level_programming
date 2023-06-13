47442cc64f9672e655464e1111af2b6eaafbb68b
Monarch <monarchofaether@gmail.com>
2023-06-14 01:14:56 +0200

    6-cap_string.c - function that capitalizes a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * *cap_string - Capitalizes each word in a string
+ * @str: string argument
+ * Return: Returns capitalized character
+ */
+
+char *cap_string(char *str)
+{
+	int i, a, j;
+	char arr[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"',
+		'(', ')', '{', '}'};
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j <= 13; j++)
+		{
+			if (str[i] == arr[j])
+			{
+				if (i != 0)
+					a = i + 1;
+				if (str[a] >= 'a' && str[a] <= 'z')
+				{
+					str[a] = str[a] - 32;
+					a = i;
+				}
+			}
+		}
+	}
+
+	return (str);
+}
