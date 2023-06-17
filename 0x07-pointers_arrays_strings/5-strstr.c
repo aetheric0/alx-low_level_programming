dac1355d41d8edfe583469bfe95061511b6f7758
Monarch <monarchofaether@gmail.com>
2023-06-17 10:43:55 +0200

    condition for empty string included in code

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,9 +12,12 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
+	j = 0;
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; needle[j] != '\0'; j++)
+		if (needle[j] == '\0')
+			return (haystack + i);
+		for (; needle[j] != '\0'; j++)
 		{
 			if (haystack[i] ==  ' ')
 			{
