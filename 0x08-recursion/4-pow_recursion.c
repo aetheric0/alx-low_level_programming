421121821c0f91e7ca6c16c1bcbf78c2640027af
Monarch <monarchofaether@gmail.com>
2023-09-27 13:11:11 +0100

    code written to find the x raised to the power of y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,2 +1,20 @@
 #include "main.h"
 
+/**
+ * _pow_recursion - Returns the value of x raised to the power y.
+ * @x: Base number
+ * @y: Exponent
+ * Return: Returns the value of x ^ y
+ **/
+
+int _pow_recursion(int x, int y)
+{
+	if (x == 0)
+		return (0);
+	if (y < 0 )
+		return (-1);
+	if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,4 @@
 int _putchar(char);
 int _strlen_recursion(char *s);
 int factorial(int);
+int _pow_recursion(int x, int y);
