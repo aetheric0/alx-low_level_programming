858592a227c72a4fe0fa3a20b00a114477e29e56
Monarch <monarchofaether@gmail.com>
2023-09-29 22:59:17 +0100

    betty compliant

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,34 +3,35 @@
 #include <ctype.h>
 
 /**
- * main - Program that prints number of arguments passed into it.
+ * main - Program that adds positive numbers.
  * @argc: argument count
  * @argv: argument vector
- * Return: 0 is Success!
+ * Return: 0 if successful, 1 otherwise.
  **/
-
 int main(int argc, char *argv[])
 {
-	int i;
-	int add = 0;
+    int i;
+    int add = 0;
 
-	if (argc == 1)
-		printf("0\n");
-	else
-		{
-			for (i = 1; i < argc; i++)
-	{
-		if (isdigit(*argv[i]))
-		{
-			add += atoi(argv[i]);
-		}
-		else
-		{
-			printf("Error\n");
-			return (1);
-		}
-	}
-			printf("%d\n", add);
-		}
-	return (0);
+    if (argc == 1)
+    {
+        printf("0\n");
+    }
+    else
+    {
+        for (i = 1; i < argc; i++)
+        {
+            if (isdigit(*argv[i]))
+            {
+                add += atoi(argv[i]);
+            }
+            else
+            {
+                printf("Error\n");
+                return (1);
+            }
+        }
+        printf("%d\n", add);
+    }
+    return (0);
 }
