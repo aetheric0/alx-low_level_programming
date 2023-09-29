9efc79a30d632f4f5a09321fed13fcda5ac0ea49
Monarch <monarchofaether@gmail.com>
2023-09-29 23:47:24 +0100

    standard library created, all source files and object files included

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncpy - function to copy a string
+ * @dest: argument string is copied to
+ * @src: argument containing string we want copied
+ * @n: max byte of src
+ * Return: Returns the copied string
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
