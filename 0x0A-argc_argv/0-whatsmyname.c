7d813ebc69a7026be6dffa3f224c165df54fd5cb
Monarch <monarchofaether@gmail.com>
2023-09-29 14:52:51 +0100

    argc condition changed to > 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,15 +2,15 @@
 
 /**
  * main - Program that prints its name
- @argc: argument count
- @argv: array of string arguments
- Return: Success (0);
+ * @argc: argument count
+ * @argv: array of string arguments
+ * Return: Success (0);
  **/
 
 int main(int argc, char *argv[])
 {
 	int i;
-	if (argv[0] && argc == 1)
+	if (argv[0] && argc > 0)
 	{
 		for (i = 0; argv[0][i] != '\0'; i++)
 			_putchar(argv[0][i]);
