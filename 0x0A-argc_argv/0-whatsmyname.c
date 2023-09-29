2bbc4deeee1d71d3ba26e642c2fac4c5462c8de7
Monarch <monarchofaether@gmail.com>
2023-09-29 14:48:41 +0100

    __attribute__ ((unused)) removed, argc used

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,10 +7,10 @@
  Return: Success (0);
  **/
 
-int main(int __attribute__ ((unused)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int i;
-	if (argv[0])
+	if (argv[0] && argc == 1)
 	{
 		for (i = 0; argv[0][i] != '\0'; i++)
 			_putchar(argv[0][i]);
