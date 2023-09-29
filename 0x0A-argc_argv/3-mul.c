32c91f58f0ee6227709b3e378d93f5bf7beebebf
Monarch <monarchofaether@gmail.com>
2023-09-29 22:03:16 +0100

    program to multiply arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Program that prints number of arguments passed into it.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 is Success!
+ **/
+
+int main(int argc, char *argv[])
+{
+	int i = 1;
+	int MUL = 1;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (; i < argc; i++)
+	{
+		MUL *= atoi(argv[i]);
+	}
+	printf("%d\n", MUL);
+	return (0);
+}
