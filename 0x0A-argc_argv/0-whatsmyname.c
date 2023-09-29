93f95d1485d0fe8699ea9402655eaee815eacd55
Monarch <monarchofaether@gmail.com>
2023-09-29 14:08:25 +0100

    program that prints its name even when the name is changed

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints its name
+ @argc: argument count
+ @argv: array of string arguments
+ Return: Success (0);
+ **/
+
+int main(int argc,char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
