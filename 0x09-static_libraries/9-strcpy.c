4625f11eb5a468c4fc00f1af15a5aa37ddda9681
Monarch <monarchofaether@gmail.com>
2023-09-30 03:13:43 +0100

    9-strcpy.c logic written in full

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
-/** _strcpy - program to copy string
- * @dest: bytes copied toa
- * @src: bytes copied from
- * Return: Returns pointer to char
+/**
+ * _strcpy - copies the string including the null terminator
+ * @dest: destination string is copied to
+ * @src: source it is copied from
+ * Return: 0 is Success!
  **/
 
 char *_strcpy(char *dest, char *src)
 {
-	*dest  = 'u';
-	src = dest;
-	char *ptr = src;
+	int i;
 
-	return (ptr);
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
 }
