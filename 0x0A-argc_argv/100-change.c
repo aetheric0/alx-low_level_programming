4d13dfb36046a8756a32e373f1100f80dd40cc14
Monarch <monarchofaether@gmail.com>
2023-10-01 20:10:22 +0100

    program to measure the minimum number of coins required to complete an amount of money using  a fixed array of numbers

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,7 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdlib.h>
-#include <ctype.h>
+
+/**
+ * convert - Converts input string to integer
+ * @s: string to be converted
+ * Return: Returns converted string
+ **/
+
+int convert(char *s)
+{
+	int num = atoi(s);
+
+	return (num);
+}
+
+/**
+ * change - Converts input string to integer
+ * @num: converted input from terminal
+ * Return: Returns change
+ **/
+
+
+int change(int num)
+{
+	const int arr[] = {1, 2, 5, 10, 25};
+	int i, count = 0;
+
+	while (num > 0)
+	{
+		for (i = 4; i >= 0; i--)
+		{
+			if (num >= arr[i])
+			{
+				num -= arr[i];
+				count++;
+				break;
+			}
+		}
+	}
+
+	return (count);
+}
 
 /**
  * main - Program that prints how much change is needed to the number
@@ -9,21 +48,21 @@
  * @argv: argument vector
  * Return: 0 if successful, 1 otherwise.
  **/
+
+
 int main(int argc, char *argv[])
 {
-	int i;
+	int num, ans;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	for (i = 0; argv[1][i] != '\0'; i++)
-	{
-		if(atoi(argv[1]) < 0)
-			{
-				printf("0\n");
-			}
-	}
+	num = convert(argv[1]);
+	ans = change(num);
+
+	printf("%d\n", ans);
+
 	return (0);
 }
