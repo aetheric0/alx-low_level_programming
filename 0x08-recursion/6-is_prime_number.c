2ba3a23f9f77e8ac520a1a2e28bb87430da869d8
Monarch <monarchofaether@gmail.com>
2023-10-01 12:02:06 +0100

    prints string passed to stdout

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * is_prime_number - Determines if n is a prime number or not
+ * @n: number argument to be determined
+ * Return: Returns 1 if TRUE and 0 if FALSE
+ **/
+
+int is_prime_number(int n)
+{
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,4 +1,6 @@
 int _putchar(char);
+void _puts_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int);
 int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
