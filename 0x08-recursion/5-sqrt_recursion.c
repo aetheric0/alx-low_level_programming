938e29c1fe2fdf2f62cb9bdfa158d4e477750aa4
Monarch <monarchofaether@gmail.com>
2023-10-01 14:17:49 +0100

    non perfect square values accounted for

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -21,10 +21,14 @@ int sqrt_helper(int n, int a)
 	if (n == 0 || n == 1)
 		return (n);
 
-	if (n/a == a && a < n)
+	if (n/a == a)
 	{
 		return (a);
 	}
+	if ( a * a > n)
+		return (-1);
 	else
+	{
 		return sqrt_helper(n, a + 1);
+	}
 }
