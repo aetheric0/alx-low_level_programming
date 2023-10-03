062f3e4fd54b343d7a845eda472f2605c24c3227
Monarch <monarchofaether@gmail.com>
2023-10-03 21:27:39 +0100

    function to print every argument of a program to stdout, each argument is on a new line

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all arguments in a program
+ * @ac: number of arguments
+ * @av: string arguments
+ * Return: On Success returns pointer to the string, Returns NULL on failure
+ **/
+
+char *argstostr(int ac, char **av)
+{
+	int len, i, j, k;
+	char *string;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	len = 0;
+	for (i = 1; i < ac; i++)
+		len += strlen(av[i]);
+	string = (char *)malloc(sizeof(char) * (len + ac));
+		if (string == NULL)
+		{
+			free(string);
+			return NULL;
+		}
+
+	k = 0;
+	for (i = 1; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			string[k] = av[i][j];
+			k++;
+		}
+		string[k] = '\n';
+		k++;
+	}
+	string[k] = '\0';
+
+	return (string);
+}
