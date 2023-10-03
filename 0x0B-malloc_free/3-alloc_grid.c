4ffe8fe48cf0b725dc9918f0475380f694c88c56
Monarch <monarchofaether@gmail.com>
2023-10-03 09:10:07 +0100

    Free memory if dynamic allocation fails

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,7 +25,7 @@ int **alloc_grid(int width, int height)
 		if (matrix[i] == NULL)
 		{
 		if (matrix[i] == NULL)
-			for(j = 0; j < width; j++)
+			for (j = 0; j < width; j++)
 			{
 				free(matrix[j]);
 			}
