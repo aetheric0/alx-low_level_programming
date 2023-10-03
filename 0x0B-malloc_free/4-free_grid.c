96ab0759a3a05d92a5c88027aa8aae263a49fc6c
Monarch <monarchofaether@gmail.com>
2023-10-03 09:18:35 +0100

    free grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - Frees the memory for the matrix alloc_grid
+ * @grid: Matrix argument
+ * @height: Matrix height
+ **/
+
+void free_grid(int **grid, int height)
+{
+	free(grid);
+}
