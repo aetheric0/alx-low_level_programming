1be0358320f0336fa97ca7cd60475b7493772f5e
Monarch <monarchofaether@gmail.com>
2023-10-05 17:41:56 +0100

    function that creates and initializes an array with the values of the range min to max

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - creates an array and initialize
+ * it with values from min to max
+ * @min: start of range
+ * @max: end of range
+ * Return: returnz pointer to an array
+ **/
+
+int *array_range(int min, int max)
+{
+	int range, i, *arr;
+
+	if (min > max)
+		return (NULL);
+	range = max - min;
+
+	arr = malloc(sizeof(int) * range);
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < range; i++, min++)
+		arr[i] = min;
+	return (arr);
+}
