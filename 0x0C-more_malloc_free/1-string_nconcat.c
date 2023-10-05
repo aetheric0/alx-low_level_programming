7b14665ff64d113e4fa60e627ea2c7760a96d068
Monarch <monarchofaether@gmail.com>
2023-10-05 14:54:49 +0100

    conditions added to check for edge cases for s1 and s2)

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,7 +19,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	if (n > strlen(s2) || n = sizeof(s2))
+	if (n > strlen(s2) || n == sizeof(s2))
 		n = strlen(s2);
 	len = strlen(s1);
 
