6ceb87424cfa18594d5b60a2c71e974b1024654d
Monarch <monarchofaether@gmail.com>
2023-10-10 00:36:09 +0100

    brackets included around nil

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,11 +12,11 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	if (d->name == NULL)
-		d->name = "nil";
-	if (d->age == 0.0)
-		d->name = "nil";
+		d->name = "(nil)";
+	if (d->age == 0)
+		d->name = "(nil)";
 	if (d->owner == NULL)
-		d->name = "nil";
+		d->name = "(nil)";
 
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
