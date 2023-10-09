e5963199e1692eebaba2ec275ea4f47c7f6eb1eb
Monarch <monarchofaether@gmail.com>
2023-10-09 22:50:48 +0100

    description and return statement added

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+/**
+ * main - printd name of file
+ * Return: (0) on success!
+ **/
 
 int main(void)
 {
-	printf("%s\n", __FILE__);
+	printf("%s", __FILE__);
+
+	return (0)
 }
