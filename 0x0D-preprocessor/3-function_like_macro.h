770986a7645f57f24aa4277aa03ec3c96ff02a24
Monarch <monarchofaether@gmail.com>
2023-10-10 00:03:59 +0100

    logic changed

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,20 +1,6 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
-/**
- * absolute - gives the absolute value
- * @x: variable
- * Return: returns absolute value
- **/
-int absolute(int);
-
-int absolute(int x)
-{
-	if (x < 0)
-		x = -1 * (x);
-	return (x);
-}
-
-#define ABS(x) absolute(x)
+#define ABS(x) ((x) < 0 ? -(x) : (x))
 
 #endif
