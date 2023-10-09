85410156382000dd2fc863177f51bbf3da438818
Monarch <monarchofaether@gmail.com>
2023-10-09 17:12:24 +0100

    betty declaration added

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,6 +2,7 @@
 
 /**
  * init_dog - initializes a variable of type dog when called
+ * @d: variable to be initialized
  * @name: name of dog
  * @age: age of dog
  * @owner: Dog owner
@@ -9,7 +10,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d -> name = name;
-	d -> age = age;
-	d -> owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
