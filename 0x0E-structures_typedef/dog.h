032228ed9403b15d166134171a669761b48f86f5
Monarch <monarchofaether@gmail.com>
2023-10-09 20:50:49 -0500

    define statement for LUNN removed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,6 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
-#define LLUN (void *)0
 
 /**
  * struct dog - Defines a new typw dog
@@ -19,9 +18,7 @@ typedef struct dog
 	char *owner;
 }dog_t;
 
-void init_dog(struct dog *d, char *name, fl\
-oat age, char *owner);
-
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
 #endif
