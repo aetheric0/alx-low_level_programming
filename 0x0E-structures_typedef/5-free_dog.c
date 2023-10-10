e2d99fcda2692dd14f4559577095611b3df39850
Monarch <monarchofaether@gmail.com>
2023-10-09 21:56:37 -0500

    program that frees dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Frees the memory of dogs
+ * @d - variable to be freed
+ **/
+
+void free_dog(dog_t d);
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->age);
+	free(d->owner);
+
+	free(d);
+}
