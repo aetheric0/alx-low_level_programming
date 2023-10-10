ee16cdc89af1707e768984057979e048a8db52ca
Monarch <monarchofaether@gmail.com>
2023-10-10 07:03:04 +0100

    betty enforced in dog.g

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,10 +17,9 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+} dog_t;
 
-void init_dog(struct dog *d, char *name, fl\
-oat age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
 
