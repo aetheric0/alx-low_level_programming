7fe33be3435f6b32d173741ef1977dda2a0f1e75
Monarch <monarchofaether@gmail.com>
2023-10-11 14:41:45 +0100

    funtion to execute a function on the members of an array of size 'size'

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,7 +4,7 @@
 /**
  * print_name - Prints name as is
  * @name: name to be printed
- " @f: function pointer to print name
+ * @f: function pointer to print name
  **/
 
 void print_name(char *name, void (*f)(char *))
