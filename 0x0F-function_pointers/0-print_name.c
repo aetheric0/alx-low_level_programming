d6375740f03a34b850a6679b04cbccc455ff1170
Monarch <monarchofaether@gmail.com>
2023-10-11 13:15:46 +0100

    function to print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_name - Prints name as is
+ * @name: name to be printed
+ " @f: function pointer to print name
+ **/
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
