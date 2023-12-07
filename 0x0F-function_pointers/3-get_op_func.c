66df9dc3d610900c6eac14f4b82108c5d109f545
Monarch <monarchofaether@gmail.com>
2023-12-07 18:03:35 +0300

    testing if else if statements work

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,6 @@
 #include "3-calc.h"
 #include <stddef.h>
+#include <string.h>
 
 /**
  * get_op_func - Retrieves operator to determine operation to be carried
@@ -9,16 +10,12 @@
 
 int (*get_op_func(char *s))(int a, int b)
 {
-	int i;
 	op_t ops[] = {{"+", op_add}, {"-", op_sub},
 		      {"*", op_mul}, {"/", op_div},
 		      {"%", op_mod}};
-
-	for (i = 0; i < (int)(sizeof(ops)/sizeof(ops[0])); i++)
-	{
-		if (*s == *(ops[i].op) && s[1] == '\0')
-			return ((ops[i].f));
-	}
-
+	if (strcmp(s, "+"))
+		return ops[0].f;
+	else if (strcmp(s, "-"))
+		return ops[1].f;
 	return (NULL);
 }
