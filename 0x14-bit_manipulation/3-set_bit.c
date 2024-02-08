9a047f49f17397060baeb04d57b401545a584853
Monarch <monarchofaether@gmail.com>
2024-02-08 19:18:05 +0300

    conflicting type errors resolved, main.h file updated with function

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,7 +7,7 @@
  * Return: 1 on success!, -1 on failure
  **/
 
-int set_bit(unsigned long int n, unsigned long int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int mask;
 
