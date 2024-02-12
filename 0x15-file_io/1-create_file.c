82c5d98c260339534965ac4b35dc027b75a98170
Monarch <monarchofaether@gmail.com>
2024-02-12 23:57:06 +0300

    nullbyte moved

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,14 +29,13 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		written = write(fd, text_content, length);
+		written = write(fd, &nullbyte, 1);
 		if (written == -1)
 		{
 			close(fd);
 			return (-1);
 		}
 	}
-
-	write(fd, &nullbyte, 1);
 
 	close(fd);
 	return (1);
