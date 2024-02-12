8c96fed377d266d3c40f4853dfbecf7e06fc9aff
Mornarch <monarchofaether@gmail.com>
2024-02-13 00:23:55 +0300

    length modified

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 {
 	ssize_t length, fd, written;
 
-	length = strlen(text_content) + 1;
+	length = strlen(text_content);
 	if (filename == NULL)
 		return (-1);
 
