395874c3f77b9d7bdcc0093f3289107217a5d5d5
Monarch <monarchofaether@gmail.com>
2024-02-13 05:09:23 +0300

    final push

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
 
 /**
  * append_text_to_file - appends text to file
@@ -11,16 +12,15 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, w;
+	int fd, w, len;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-	while (text_content != NULL)
-       		w = write(fd, text_content, 1);
-	if (w == -1)
+	len = strlen(text_content);
+	if ((w = write(fd, text_content, len)) == -1)
 		return (-1);
 
 	close(fd);
