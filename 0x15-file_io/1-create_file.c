40587240d94c3c00d0bdb5e95722d17619ed91a8
Monarch <monarchofaether@gmail.com>
2024-02-14 00:15:21 +0300

    checking if there's anything to push

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,16 +18,25 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = creat(filename, 0600);
-	if (fd == -1)
+	if ((access(filename, F_OK)) != 0 )
 	{
-		return (-1);
+		fd = open(filename, O_RDWR, 0600);
+		if (fd == -1)
+			return (-1);
 	}
-
-	if ((write(fd, text_content, BUFFSIZE)) == -1)
+	else
 	{
-		close(fd);
-		return (-1);
+		fd = open(filename, O_RDWR | O_TRUNC);
+		if (fd == -1)
+			return (-1);
+	}
+	if (text_content != NULL)
+	{
+		if ((write(fd, text_content, BUFFSIZE)) == -1)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
 
 	close(fd);
