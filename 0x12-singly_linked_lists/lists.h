06a93835c6c6addcb887cee4994fb34b38722eee
mornark <chrixiants@gmail.com>
2024-03-06 07:12:20 +0300

    bettyfied

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,12 +2,19 @@
 #define HEADER_FILE
 #include <string.h>
 
+/**
+ * struct list_t - structure for a linked list
+ * @str: string variable
+ * @len: number
+ * @next: pointer to next node
+ **/
+
 typedef struct list_t
 {
 	char *str;
 	int len;
 	struct list_t *next;
-}list_t;
+} list_t;
 
 size_t print_list(const list_t *h);
 
