97131a094de1dd0aa978823826e7dcdb6e9a8048
mornark <chrixiants@gmail.com>
2024-03-23 17:43:59 +0300

    sum data part of a list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint(dlistint_t *head)
+ * @head: pointer to the list
+ * Return: return sum of of data part in list on success, 0 if empty
+ **/
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+	dlistint_t *temp;
+
+	temp = head;
+	if (temp == NULL)
+		return (0);
+	while (temp->next != NULL)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+
+	return (sum);
+}
