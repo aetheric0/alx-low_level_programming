a680704545faccb2bf82c371a4840e704cb06eee
mornark <chrixiants@gmail.com>
2024-03-26 06:42:12 +0300

    dynamic library linked to python

---
+++ 0x18-dynamic_libraries/ops/main.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+int add(int, int);
+int sub(int, int);
+int mul(int, int);
+int div(int, int);
+int mod(int, int);
+
+#endif
---
+++ 0x18-dynamic_libraries/ops/operations.c
@@ -0,0 +1,60 @@
+#include "main.h"
+
+/**
+ * add - Adds two numbers
+ * @a: first operand
+ * @b: second operand
+ * Return: Result of operation
+ **/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - Subtract two numbers
+ * @a: first operand
+ * @b: second operand
+ * Return: Result of operation
+ **/
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - Multiplies two numbers
+ * @a: first operand
+ * @b: second operand
+ * Return: Result of operation
+ **/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - Divides two numbers
+ * @a: first operand
+ * @b: second operand
+ * Return: Result of operation
+ **/
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - Remainder of division
+ * @a: first operand
+ * @b: second operand
+ * Return: Result of operation
+ **/
+int mod(int a, int b)
+{
+	return (a % b);
+}
