7cf5e9b73bb5e8a390d58d6bc0e70fad5e22a7e5
mornark <chrixiants@gmail.com>
2024-03-26 06:07:23 +0300

    script to create dynamic library

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -6,6 +6,8 @@ int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
+void _init(void);
+void _fini(void);
 int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
