f87a42a35e355f66804932002fc20672186a1deb
Monarch <monarchofaether@gmail.com>
2024-05-29 22:59:45 +0300

    diagonals corrected

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,13 +4,13 @@
 
 def island_perimeter(grid):
     """Finds the perimeter of an island except for diagonal cells"""
-    perimeter = 0
+    perimeter = k = 0
     for i in range(len(grid)):
         for j in range(len(grid[i]) - 1):
             if grid[i][j] == 1:
                 perimeter += 4
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2
+                if i > 0 and grid[i - 1][j - 1] == 1:
+                    perimeter -= 4
+                if j > 0 and grid[i + 1][j + 1] == 1:
+                    perimeter -= 4
     return perimeter
