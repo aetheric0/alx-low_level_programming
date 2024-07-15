6916ed9c6a7345591c1bf1040636978ad45fcd60
Monarch <monarchofaether@gmail.com>
2024-07-15 10:21:50 +0300

    search algorithms

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - Uses linear search algorithm to search for a number in
+ * given array
+ * @array: array to be searched
+ * @size: number of elements in array
+ * @value: value to search
+ * Return: The index of array on success, NULL on failure
+ **/
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (value == array[i])
+			return (i);
+	}
+
+	return (-1);
+}
